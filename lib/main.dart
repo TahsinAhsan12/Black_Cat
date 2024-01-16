288b8b2e24c33645673df5f5f0e005df90b2a35b
TahsinAhsan12 <155448120+TahsinAhsan12@users.noreply.github.com>
2024-01-16 16:35:03 +0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,135 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-void main() {
-  runApp(const MyApp());
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'login_block.dart';
+
+void main(){
+runApp(LoginScreen());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context  {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        SizedBox(width: double.infinity,),
-      SizedBox(height:60,),
-        onPressed: (){
-
-    },
-    cardColor: Colors.indigoAccent[400],
-    shape: RoundedRectangleBorder(
-    side: BorderSide(
-    color: Colors.black,
-    ),
-    borderRadius: BorderRadius.circular(40)
-    ),
-    child: const Column()
-      Text("Login",style: TextStyle(
-    fontWeight: FontWeight.w600,fontSize: 16,color: Colors.white70
-
-    )
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({
+    Key ? key
+  }): super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginScreenState extends State < LoginScreen > {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final loginBloc = BlocProvider.of < LoginBloc > (context);
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Login Screen'),
       ),
-      body: SafeArea(
-      child: Container(
-      width: double.infinity,
-      height: MediaQuery.of(context).size.height,
-      padding: EdgeInsets.symmetric(horizontal: 30,vertical: 30),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-        Column
-    (
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            "Hello There!",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-          ),
-          SizedBox(height: 30,),
-          Text("Automatic identity verification which enable you to verify your identity",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.grey[700],
-                fontSize: 15
-            ),
-          ),
-        ],
+      body: BlocListener < LoginBloc, LoginState > (
+        listener: (context, state) {
+          if (state is LoginFailure) {
+            ScaffoldMessenger.of(context)..hideCurrentSnackBar()..showSnackBar(SnackBar(
+              content: Text(state.error),
+              duration: const Duration(seconds: 3),
+            ));
+          }
+        },
+        child: BlocBuilder < LoginBloc, LoginState > (
+          builder: (context, state) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: < Widget > [
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: state is!LoginLoading ?
+                        () {
+                      loginBloc.add(
+                        LoginButtonPressed(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                        ),
+                      );
+                    } :
+                    null,
+                    child: const Text('Login'),
+                  ),
+                  if (state is LoginLoading)
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: CircularProgressIndicator(),
+                    ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
-        MaterialButton(
-        minWidth: double.infinity,
-    height:60,
-    onPressed: (){
-
-    },
-    color: Colors.redAccent,
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(40)
-    ),
-    child: Text("Sign UP",style: TextStyle(
-    fontWeight: FontWeight.w600,fontSize: 16,
-
-    ),),
-    ),
-    ],
-    ),
-
-    ),
     );
   }
-
+}
