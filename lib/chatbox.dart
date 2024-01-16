55aec9afc85448fbfc2bc7db4ca6ac4dc7609d77
rafiul-siddique <rafiulsiddique5@gmail.com>
2024-01-16 09:48:13 +0600

    new_chat

---
+++ lib/chatbox.dart
@@ -0,0 +1,62 @@
+return Scaffold(
+      body: Container(
+        child: Center(child: Text("Chat")),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            title: Text("Chats"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group_work),
+            title: Text("Channels"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            title: Text("Profile"),
+          ),
+        ],
+      ),
+    );
+return Scaffold(
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SafeArea(
+              child: Padding(
+                padding: EdgeInsets.only(left: 16,right: 16,top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Conversations",style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold),),
+                    Container(
+                      padding: EdgeInsets.only(left: 8,right: 8,top: 2,bottom: 2),
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.pink[50],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.add,color: Colors.pink,size: 20,),
+                          SizedBox(width: 2,),
+                          Text("Add New",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
